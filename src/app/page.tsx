0934b60112e06b28cb0e529eb3c41ec14c03b3f0
cabarkapa99 <dimitrije.cabarkapa99@gmail.com>
2025-04-22 13:10:34 +0200

    small change url redirect fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+            href="/board"
             rel="noopener noreferrer"
           >
             Go To Board
